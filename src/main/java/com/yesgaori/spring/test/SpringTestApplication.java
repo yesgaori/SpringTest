9d4ba19ca19290148807f2a4dfb0014c1127e2b8
khy20100065@gmail.com <khy20100065@gmail.com>
2023-08-21 16:22:36 +0900

    [강하영]  - 기본설정 재확립

--- src/main/java/com/yesgaori/spring/test/SpringTestApplication.java
+++ src/main/java/com/yesgaori/spring/test/SpringTestApplication.java
@@ -1,4 +1,4 @@
-package com.yesgaori.test;
+package com.yesgaori.spring.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- src/main/java/com/yesgaori/spring/test/hello/HelloController.java
+++ src/main/java/com/yesgaori/spring/test/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.yesgaori.test.hello;
+package com.yesgaori.spring.test.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
